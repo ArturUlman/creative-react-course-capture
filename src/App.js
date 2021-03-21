2647ff9a2922ca015f02089b44c7979f1a664cf5
Artur <arteqx@gmail.com>
2021-03-21 22:37:32 +0000

    5. Styled Comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+//global style
+
 import AboutSection from "./components/AboutSection";
+import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
